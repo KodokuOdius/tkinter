eb26336dd5b18fc671d73770b0827583fd89065d
KodokuOdius <80702757+KodokuOdius@users.noreply.github.com>
2021-10-29 11:05:48 +0300

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,11 +1,12 @@
-
+import random
 from tkinter import *
 
 """
     !!!!!!!!!!!!!!!!!!!!!!!
-        ЖРЁТ ПАМЯТЬ
-        ЖДЁТ ОПЕРАТИВКУ
-        ЖРЁТ ЛЮБИМУЮ RAM
+        ДОБАВИТЬ ЛОГИКУ НЕПРЕРЫВНОГО ДВИЖЕНИЯ
+        ШОБЫ ДВИГАЛОСЬ
+        НУ ТЫ ПОНЯЛ 
+        А ЕЩЁ ЛОГИКУ УДЛИНЕНИЯ
         ИСПРАВИТЬ
     !!!!!!!!!!!!!!!!!!!!!!!
 """
@@ -13,116 +14,79 @@ from tkinter import *
 
 
 window = Tk()
-window.title("Practical")
+window.title("Snakei I")
 window.resizable(False, False)
 window["bg"] = "black"
-fieldX = 25
-fieldY = 25
-i = 0
+fieldX = 11
+fieldY = 11
 listX = []
-while i != fieldX:
-    listX.append(i)
-    i += 1
-i = 0
 listY = []
-while i != fieldX:
-    listY.append(i)
-    i += 1
-
+for i in range(fieldX):
+    listX.append(i)
+    for j in range(fieldY):
+        listY.append(i)
 
 window.rowconfigure(listX, minsize=10, weight=1)
 window.columnconfigure(listY, minsize=10, weight=1)
 
-i = 0
-while i != fieldX:
-    j = 0
-    while j != fieldY:
-        Label(
+del listX
+del listY 
+
+Field = []
+temp_field = []
+
+for i in range(fieldX):
+    for j in range(fieldY):
+        temp_field.append(Label(
             master=window,
             bg="black",
-            relief="ridge",
+            # relief="ridge",
             width = 2
-        ).grid(row=i, column=j, sticky="swen")
-        j += 1
-    i += 1
+        ))
+        temp_field[j].grid(row=i, column=j, sticky="swen")
+    Field.append(temp_field)
+    temp_field = []
 
-
+del temp_field
 headX = fieldX//2
-headY = fieldY//2 
-Label(
-    master=window,
-    bg="green",
-    relief="ridge",
-    width = 2
-).grid(row=headX, column=headY, sticky="swen")
+headY = fieldY//2
+
+Field[headX][headY]["bg"] = "green"
+
+appleX = random.randint(0, fieldX - 1)
+appleY = random.randint(0, fieldY - 1)
+while appleX == headX and appleY == headY:
+    appleX = random.randint(0, fieldX - 1)
+    appleY = random.randint(0, fieldY - 1)
+Field[appleX][appleY]["bg"] = "red"
 
 def move_up():
     global headX
     if headX != 0:
-        Label(
-            master=window,
-            bg="black",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "black"
         headX -= 1
-        Label(
-            master=window,
-            bg="green",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "green"
 
 def move_down():
     global headX
     if headX != fieldX - 1:
-        Label(
-            master=window,
-            bg="black",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "black"
         headX += 1
-        Label(
-            master=window,
-            bg="green",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "green"
 
 def move_left():
     global headY
     if headY != 0:
-        Label(
-            master=window,
-            bg="black",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "black"
         headY -= 1
-        Label(
-            master=window,
-            bg="green",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "green"
 
 def move_right():
     global headY
     if headY != fieldY - 1:
-        Label(
-            master=window,
-            bg="black",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "black"
         headY += 1
-        Label(
-            master=window,
-            bg="green",
-            relief="ridge",
-            width = 2
-        ).grid(row=headX, column=headY, sticky="swen")
+        Field[headX][headY]["bg"] = "green"
 
 
 def handle_keypress(event):
@@ -135,7 +99,7 @@ def handle_keypress(event):
     elif event.char == "d":
         move_right()
     elif event.char == "x":
-        window.destroy
+        exit()
 
     
     
